c3d1a7d72433769ebd900a422ae74fd721cdcf0a
fvillagran <gabriel.villagran@gmail.com>
2023-05-22 22:27:03 -0400

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -18,7 +18,9 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        #g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = redis.Redis(host='redis', port=6379, username='redis', password='redis_password')
+        g.redis.ping()
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
